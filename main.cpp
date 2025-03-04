e6e06c03025abdc0f65a2057b3e0270b90708003
xm200 <glebf72@gmail.com>
2025-03-04 18:14:46 +0300

    main: add graph with bfs and dfs

--- main.cpp
+++ main.cpp
@@ -1,18 +1,49 @@
-#include <bits/stdc++.h>
-#include "interval.h"
+#include <iostream>
+#include <interval.h>
+#include <queue>
+#include <vector>
+
 
 template <class T>
 struct Node {
-    interval::interval<T> interval;
-    Node() = default;
-    ~Node() = default;
-    Node(interval::interval<T> _interval) : interval(_interval) {}
-
+    interval::interval<T> range; // Node With code and interval
 };
 
+struct Graph {
+    std::vector<int> ptrs; // pointers on Nodes
+    std::vector<bool> used; // Array of used Nodes
+    std::vector<std::vector<int>> g;
 
-int main() {
-    Node<int> test;
-    test.interval.print();
-    return 0;
-}
+    Graph(int v) {
+        ptrs.resize(v);
+        used.resize(v);
+        g.resize(v);
+    }
+
+    void _dfs(int start) {
+        if (used[start]) return;
+        used[start] = true;
+        for (int &x: g[start]) _dfs(x);
+    }
+
+    void dfs(int start) {
+        for (int i = 0; i < used.size(); ++i) used[i] = false;
+        _dfs(start)
+    }
+
+    void _bfs(int start) {
+        std::queue<int> q;
+        q.push(start);
+        while (!q.empty()) {
+            int v = q.pop();
+            if (used[v]) continue;
+            used[v] = true;
+            for (int x : g[start]) q.push(x);
+        }
+    }
+
+    void bfs(int start) {
+        for (int i = 0; i < used.size(); ++i) used[i] = false;
+        _bfs(start);
+    }
+};
