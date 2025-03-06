e101338f3b4d2a842d00f2294ab3a3cbb3899608
xm20 <glebf72@gmail.com>
2025-03-06 14:04:23 +0300

    Added parser base

--- CFGparser.h
+++ CFGparser.h
@@ -38,17 +38,22 @@ namespace parse {
         #endif
     }
     struct cache_t {
+
         void init(const std::string &way) {
             if (inited) {
                 if (way + '/' == path || way == path) return;
+#if defined(DEBUG_MODE)
                 throw std::logic_error("cache with multiple initialization and different paths");
+#endif
             }
+
             if (way.empty()) path = way;
             else path = way + '/';
             path += "cache/";
             system(("mkdir " + path).c_str());
             inited = true;
         }
+
         ~cache_t() {
 #if !defined(DO_NOT_REMOVE_CACHE)
             if (!inited) return;
@@ -57,9 +62,12 @@ namespace parse {
 #endif
         }
         const std::string &operator()() const {
+#if defined(DEBUG_MODE)
             if (!inited) throw std::logic_error("cache dir don`t exists");
+#endif
             return path;
         }
+
     private:
         std::string path;
         bool inited = false;
@@ -67,6 +75,7 @@ namespace parse {
 
     std::vector<std::string> compiler_command(const std::string &path) {
         std::size_t ind = 0, point = path.size();
+
         for (auto i = path.size(); i --> 0;) {
             if (point == path.size() && path[i] == '.') point = i;
             if (path[i] == '/' || path[i] == '\\') {
@@ -74,6 +83,7 @@ namespace parse {
                 break;
             }
         }
+
         cache.init(path.substr(0, ind));
         std::string buf;
         std::vector<std::string> out;
@@ -81,16 +91,35 @@ namespace parse {
         std::ifstream file(path);
         while (std::getline(file, buf)) out.push_back(buf);
         file.close();
+
 #if defined(VERBOSE)
         std::cout << "path to cache: " << cache() << '\n';
         std::cout << "file name: " << file_name << '\n';
         std::cout << "file type: " << (file_type.empty() ? "*none*":file_type) << '\n';
         std::cout << "code have " << out.size() << " lines\n";
 #endif
+
         return out;
     }
 
     class Parser {
+        [[nodiscard]] static size_t get_spaces(const std::string &buf) {
+            size_t spaces = 0;
+            for (auto &s : buf) if (s == ' ') ++spaces; else break;
+            return spaces;
+        }
+
+        static size_t get_code_block(const std::vector<std::string> &code, size_t l = 0) {
+#if defined(DEBUG_MODE)
+            if (l >= code.size()) throw
+                    std::length_error("Unreachable start limit in function get_code_block()");
+#endif
+            size_t block_spaces = get_spaces(code[l]);
+            for (size_t i = l; i < code.size(); ++i)
+                if (get_spaces(code[i]) < block_spaces) return i - l;
+            return code.size() - l;
+        }
+
 
     };
 }
