4f016f256002acf5ee682e5ec5713272e21695d0
Fedoracora <valyanskiyfedor@gmail.com>
2025-03-25 14:19:38 +0300

    initial update

--- ASTExperiment.h
+++ ASTExperiment.h
@@ -19,13 +19,13 @@ namespace custom {
 template<typename> \
 __VA_ARGS__; \
 __VA_ARGS__ { \
-switch(get_type(a)) {
+switch(get_in_type(a)) {
 #define init_struct_cession(a, ...) \
 __VA_ARGS__ { \
-switch(get_type(a)) {
-#define enable_int(name, ...) case INT: return name<typeInt>(__VA_ARGS__);
-#define enable_float(name, ...) case FLOAT: return name<typeFloat>(__VA_ARGS__);
-#define enable_string(name, ...) case STRING: return name<std::string>(__VA_ARGS__);
+switch(get_in_type(a)) {
+#define enable_int(name, ...) case custom::custom_type::types::INT: return name<typeInt>(__VA_ARGS__);
+#define enable_float(name, ...) case custom::custom_type::types::FLOAT: return name<typeFloat>(__VA_ARGS__);
+#define enable_string(name, ...) case custom::custom_type::types::STRING: return name<std::string>(__VA_ARGS__);
 #define close_cession(T) \
 default: throw std::runtime_error("unknown type");}} \
 template<typename T>
@@ -49,11 +49,10 @@ enable_string(name, __VA_ARGS__)
 
         [[maybe_unused]] vec_line(const vec_line &data) : vec(data.vec), l(data.l), len(data.len) {}
 
-        [[maybe_unused]] vec_line(const S *vec_, std::size_t l_, std::size_t len_) : vec(vec_), l(l_), len(len_) {}
+        [[maybe_unused]] explicit vec_line(const S &data) : vec(&data), len(data.size()) {}
 
-        [[maybe_unused]] explicit vec_line(S data) : vec(&data) {
-            len = data.size();
-        }
+        [[maybe_unused]] vec_line(const S *vec_, const std::size_t l_, const std::size_t len_) :
+                vec(vec_), l(l_), len(len_) {}
 
         [[maybe_unused]] [[nodiscard]] std::size_t size() const _GLIBCXX_NOEXCEPT { return len; }
 
@@ -205,9 +204,16 @@ enable_string(name, __VA_ARGS__)
         while (other[l] == ' ' && l < other.size()) ++l;
         if (l == other.size()) throw std::logic_error("empty string if function erase_spaces");
         while (other[r - 1] == ' ' && r > 0) --r;
-        return other.substr(l, other.size() - r);
+        return other.substr(l,  r);
     }
 
+
+    struct custom_type;
+    [[nodiscard]] static short get_in_type(const std::variant<
+                interval::interval<typeInt>,
+                interval::interval<typeFloat>,
+                interval::interval<std::string>,
+                std::vector<custom_type*>> &d);
 
 
     struct custom_type {
@@ -223,17 +229,41 @@ enable_string(name, __VA_ARGS__)
             INT, FLOAT, STRING, VECTOR, UNKNOWN
         };
 
+
+        template<typename type>
+        [[nodiscard]] static bool can_cast(const inner_type &d) {
+            return std::get_if<type>(&d) != nullptr;
+        }
+
+        [[nodiscard]] static short extract_type_from_string(const std::string &s) {
+            if ((s.front() == '\'' && s.back() == '\'') || (s.front() == '\"' && s.back() == '\"')) return STRING;
+            bool has_dot = false;
+            if (s.size() == 1 && s.front() == '.') return UNKNOWN;
+            for (const auto chr: s) {
+                if (chr == '.') {
+                    if (has_dot) throw std::runtime_error("Function get_type() : double dot in integer-like type!");
+                    has_dot = true;
+                }
+                else if (chr > '9' || chr < '0') return UNKNOWN;
+            }
+            return (has_dot) ? FLOAT : INT;
+        }
+
         short type = UNKNOWN;
         inner_type data;
-        str_type name{};
+        std::string name{};
 
         custom_type() = default;
-        explicit custom_type(const inner_type& d) : type(get_type(d)), data(d) {}
+        explicit custom_type(const inner_type& d) : type(get_in_type(d)), data(d) {}
 
         ~custom_type() = default;
 
+
+
+        void reset_type() { type = get_in_type(data); }
+
 #define subst(fun, a, b) \
-    switch (get_type(a)) { \
+    switch (get_in_type(a)) { \
         case INT: return fun<typeInt>(a, b); \
         case FLOAT: return fun<typeFloat>(a, b); \
         case STRING: return fun<std::string>(a, b); \
@@ -241,7 +271,7 @@ enable_string(name, __VA_ARGS__)
     }
 
 #define subst_digit_only(fun, a, b)  \
-    switch (get_type(a)) { \
+    switch (get_in_type(a)) { \
         case INT: return fun<typeInt>(a, b); \
         case FLOAT: return fun<typeFloat>(a, b); \
         default: throw std::runtime_error("unknown type"); \
@@ -264,7 +294,7 @@ enable_string(name, __VA_ARGS__)
 
         custom_type& operator=(const inner_type &a) {
             data = a;
-            type = get_type(a);
+            type = get_in_type(a);
             return *this;
         }
 
@@ -284,31 +314,14 @@ enable_string(name, __VA_ARGS__)
         }
 
 
-        [[nodiscard]] static short get_type(const std::string &s) {
-            if ((s.front() == '\'' && s.back() == '\'') || (s.front() == '\"' && s.back() == '\"')) return STRING;
-            bool has_dot = false;
-            if (s.size() == 1 && s.front() == '.') return UNKNOWN;
-            for (const auto chr: s) {
-                if (chr == '.') {
-                    if (has_dot) throw std::runtime_error("Function get_type() : double dot in integer-like type!");
-                    has_dot = true;
-                }
-                else if (chr > '9' || chr < '0') return UNKNOWN;
-            }
-            return (has_dot) ? FLOAT : INT;
-        }
-
-        [[nodiscard]] static int search_name(const variables &v, const str_type &s) {
-            for (int i = 0; i < v.size(); ++i)
+        [[nodiscard]] static std::size_t search_name(const variables &v, const str_type &s) {
+            for (std::size_t i = 0; i < v.size(); ++i)
                 if (v[i].first.extract() == s.extract()) return i;
             return -1;
         }
 
         [[nodiscard]] static std::pair<str_type, inner_type> dereference_cast(const str_type &s, const variables &v) {
-            switch (const auto _s = erase_spaces(s).extract(); get_type(_s)) {
-                case UNKNOWN: {
-                    return v[search_name(v, s)];
-                }
+            switch (const auto _s = erase_spaces(s).extract(); extract_type_from_string(_s)) {
                 case INT: {
                     interval::interval<typeInt> buf;
                     buf.add_point(std::stoll(_s));
@@ -321,11 +334,14 @@ enable_string(name, __VA_ARGS__)
                     str_type data;
                     return {data, buf};
                 }
-                default: {
+                case STRING: {
                     interval::interval<std::string> buf;
                     buf.add_point(_s);
                     str_type data;
                     return {data, buf};
+                }
+                default: {
+                    return v[search_name(v, s)];
                 }
             }
         }
@@ -382,7 +398,7 @@ enable_string(name, __VA_ARGS__)
 
             const auto buf = other.substr(l1, r1 - l1 + 1);
 
-            return merge(p1, p2, buf);
+            // return merge(p1, p2, buf);
         }
 
     protected:
@@ -405,22 +421,10 @@ enable_string(name, __VA_ARGS__)
         }
 
         static void checkType(const inner_type &a, const inner_type &b, const std::string &name_op) {
-            if (get_type(b) != get_type(a))
+            if (get_in_type(b) != get_in_type(a))
                 throw std::logic_error("Function operator " + name_op + ", error: could not compare different types!");
         }
 
-        template<typename type>
-        [[nodiscard]] static inline bool can_cast(const inner_type &d) {
-            return std::get_if<type>(&d) != nullptr;
-        }
-
-        [[nodiscard]] static short get_type(const inner_type &d) {
-            if (can_cast<interval::interval<typeInt>>(d)) return types::INT;
-            if (can_cast<interval::interval<typeFloat>>(d)) return types::FLOAT;
-            if (can_cast<interval::interval<std::string>>(d)) return types::STRING;
-            if (can_cast<std::vector<custom_type*>>(d)) return types::VECTOR;
-            throw std::runtime_error("Function: can_cast(), error: UNKNOWN TYPE");
-        }
 
         template<typename T>
         [[nodiscard]] static inner_type less(const inner_type &a, const inner_type &b) {
@@ -503,6 +507,149 @@ enable_string(name, __VA_ARGS__)
     };
 
 
+    [[nodiscard]] static short get_in_type(const std::variant<
+                 interval::interval<typeInt>,
+                 interval::interval<typeFloat>,
+                 interval::interval<std::string>,
+                 std::vector<custom_type*>> &d) {
+        if (custom_type::can_cast<interval::interval<typeInt>>(d)) return custom_type::types::INT;
+        if (custom_type::can_cast<interval::interval<typeFloat>>(d)) return custom_type::types::FLOAT;
+        if (custom_type::can_cast<interval::interval<std::string>>(d)) return custom_type::types::STRING;
+        if (custom_type::can_cast<std::vector<custom_type*>>(d)) return custom_type::types::VECTOR;
+        throw std::runtime_error("Function: can_cast(), error: UNKNOWN TYPE");
+
+    }
+
+}
+
+
+
+namespace ast {
+    const std::vector<std::string> inline operations =
+        {"and", "or", "==", "!=", ">", "<", ">=", "<=", "+", "-", "//", "%"};
+    struct ast_node {
+        ast_node *parent = nullptr;
+        ast_node *l = nullptr, *r = nullptr;
+        std::optional<custom::custom_type> data{};
+        std::size_t op = -1;
+
+        explicit ast_node(ast_node *p) : parent(p) {}
+        void gen(const custom::str_type &orig_s) {
+            auto s = erase_spaces(orig_s);
+
+
+            if (s.front() == '(' && s.back() == ')') {
+                std::size_t depth = 0;
+                for (auto i = 0; i < s.size() - 1; ++i) {
+                    if (s[i] == '(') ++depth;
+                    if (s[i] == ')') --depth;
+                    if (depth == 0) goto point;
+                }
+                gen(s.substr(1, s.size() - 2));
+                return;
+            }
+
+            point:
+            std::vector<std::size_t> search(operations.size(), -1);
+            std::size_t depth = 0;
+            for (auto i = 0; i < s.size(); ++i) {
+                if (s[i] == '(') ++depth;
+                if (s[i] == ')') --depth;
+                if (depth == 0) {
+                    for (auto op = 0; op < operations.size(); ++op) {
+                        if (s.substr(i, operations[op].size()).extract() == operations[op]) {
+                            search[op] = i;
+                        }
+                    }
+                }
+            }
+
+            for (auto i = 0; i < search.size(); ++i) {
+                if (search[i] != -1) {
+                    l = new ast_node(this);
+                    r = new ast_node(this);
+                    op = i;
+                    l->gen(s.substr(0, search[i]));
+                    r->gen(s.substr(search[i] + operations[i].size()));
+                    return;
+                }
+            }
+
+            //no operator found
+            const auto type = custom::custom_type::extract_type_from_string(orig_s.extract());
+            data = dereference_cast(s.extract(), type);
+
+        }
+
+        void tree() {
+            const std::string s;
+            tree(this, s);
+            std::cout << std::flush;
+        }
+
+    private:
+        [[nodiscard]] static custom::custom_type dereference_cast (const std::string &s, const short type) {
+            custom::custom_type ret;
+            switch (type) {
+                case custom::custom_type::INT: {
+                    interval::interval<typeInt> buf;
+                    buf.add_point(std::stoll(s));
+                    ret.data = buf;
+                    break;
+                }
+                case custom::custom_type::FLOAT: {
+                    interval::interval<typeFloat> buf;
+                    buf.add_point(std::stod(s));
+                    ret.data = buf;
+                    break;
+                }
+                case custom::custom_type::STRING: {
+                    interval::interval<std::string> buf;
+                    buf.add_point(s);
+                    ret.data = buf;
+                    break;
+                }
+                default: {
+                    ret.name = s;
+                    break;
+                }
+            }
+            ret.reset_type();
+            return ret;
+        }
+
+        init_struct_cession(v, static void sub_print(custom::custom_type::inner_type &v))
+            enable_all_types(sub_print, v)
+        close_cession(T)
+        static void sub_print(custom::custom_type::inner_type &v) {
+            std::cout << std::get<interval::interval<T>>(v).print();
+        }
+        static void tree(ast_node *_root, const std::string &move) {
+            if (_root->data != std::nullopt) {
+                std::cout << move;
+                sub_print(_root->data.value().data);
+                std::cout << std::endl;
+                return;
+            }
+            std::cout << move << operations[_root->op] + '\n';
+            auto move_new = move;
+            if (!move_new.empty()) {
+                for (auto i = move_new.size(); i --> move_new.size() - 4; )
+                    move_new[i] = '.';
+            }
+            move_new += "|---";
+            tree(_root->l, move_new);
+            tree(_root->r, move_new);
+        }
+    };
+
+    [[nodiscard]] inline ast_node * generate_ast(const custom::str_type &s) {
+        auto *root = new ast_node(nullptr);
+        root->gen(s);
+        return root;
+    }
+
+}
 #undef init_cession
 #undef init_struct_cession
 #undef enable_all_types
@@ -510,5 +657,4 @@ enable_string(name, __VA_ARGS__)
 #undef enable_float
 #undef enable_string
 #undef close_cession
-}
 #endif
