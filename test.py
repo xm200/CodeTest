2aeedc1bdbdb14f148e9f57d618328dabdebc062
Xm20 <87058640+xm200@users.noreply.github.com>
2025-04-11 08:38:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-a = 3
-b = a + 3
 c = int(input())
-print(c)
-
-
-if c == "a":
-    if a == 3:
-        print(c)
-    elif a == 2:
-        if c == 3:
-            print('b')
-        else:
-            print(a)
-    else:
-        print(a)
-else:
-    if a == 8:
-        print("b")
-    else:
-        print("aboba")
+d = int(input())
+if c > 3 and d > 5:
+    if c <= 99999 or not d < 225:
+        print("asd")
