ccae55c9b105645d3667d5cfa90afefdc8f8ed52
xm20 <glebf72@gmail.com>
2025-04-14 11:13:04 +0300

    mac\linux installer have became mac\linux instaler
    Signed-off-by: xm20 <glebf72@gmail.com>

--- installer_mac_linux.sh
+++ installer_mac_linux.sh
@@ -22,21 +22,38 @@ then
   exit
 fi
 
+mac=0
+
+if [ "$(uname)" == "Darwin" ]
+then
+  mac=1
+fi
+
 sub_install () {
   local tmp_path
   tmp_path=$(which "$1")
   if [ -z "$tmp_path" ]
   then
     echo -n "$1 is not installed. Install it? CodeTest would not work without it. [Y/n]: "
-    ans=""
-    read -r ans
-    if [[ "$ans" -eq "Y" ]] || [ -z "$ans" ] || [[ "$ans" -eq "y" ]]
+    if [ "$1" == "brew" ]
     then
-      sudo apt update
-      sudo apt install "$2"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
-      echo "Installation canceled by user."
-      exit 0
+      ans=""
+      read -r ans
+      if [[ "$ans" == "Y" ]] || [ -z "$ans" ] || [[ "$ans" == "y" ]]
+      then
+        if [ $mac == 0 ]
+        then
+          sudo apt update
+          sudo apt install "$2"
+        else
+          sudo brew install "$2"
+        fi
+      else
+        echo "Installation canceled by user."
+        exit 0
+      fi
     fi
   fi
   tmp_path=$(which "$1")
@@ -46,6 +63,12 @@ sub_install () {
       exit 1
   fi
 }
+
+if [ $mac == 1 ]
+then
+  sub_install "brew"
+fi
+
 
 sub_install "make" "build-essential"
 sub_install "cmake" "cmake"
