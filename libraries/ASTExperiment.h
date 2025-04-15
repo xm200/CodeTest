3aa45f89ec64067cf14c1cbbe453147470dd934b
xm20 <glebf72@gmail.com>
2025-04-15 16:09:24 +0300

    update AST
    Signed-off-by: xm20 <glebf72@gmail.com>

--- libraries/ASTExperiment.h
+++ libraries/ASTExperiment.h
@@ -765,9 +765,9 @@ namespace ast {
                     // write.push_back(i);
                     std::vector<custom::custom_type*> w = i;
                     auto buf_index = 0;
-                    for (const auto &k : w) {
+                    for (auto &k : w) {
                         if (buf_index < j.size() && k->name == j[buf_index]->name) {
-                            *k = *j[buf_index++];
+                            k = j[buf_index++];
                         }
                     }
                     tmp.insert(w);
